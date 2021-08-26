083f97f35fb9b05649d223a490258b92473ae731
khabibulloh777 <khabibullohkhalilov@gmail.com>
2021-08-26 10:37:35 +0500

    Initialize project using Create React App

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Siadbar from './Siadbar'
+import './Header.css'
+function Header() {
+    return (
+        <div>
+           <Siadbar/> 
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Siadbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Siadbar.css'
+function Siadbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Siadbar
